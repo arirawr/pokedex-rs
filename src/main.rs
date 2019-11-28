d426b37718bef9cc4bfead86f2521624d0a79b99
Ari Vaniderstine <ari.vaniderstine@embark-studios.com>
2019-11-28 14:22:01 +0100

    Refactor types

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,18 @@ struct TypeSlot {
 #[derive(Deserialize)]
 struct Type {
     name: String,
-    url: String,
 }
+
+#[derive(Deserialize, Default)]
+struct TypeEffectiveness {
+    no_damage_to: Option<Vec<Type>>,
+    no_damage_from: Option<Vec<Type>>,
+    double_damage_to: Option<Vec<Type>>,
+    double_damage_from: Option<Vec<Type>>,
+    half_damage_to: Option<Vec<Type>>,
+    half_damage_from: Option<Vec<Type>>,
+}
+
 
 fn main() {
     println!("{}", style("Pokedex CLI").bold().magenta());
@@ -44,11 +54,15 @@ fn main() {
         .get_matches();
     let input_name = matches.value_of("Pokemon Name").unwrap();
 
-    match make_request(input_name) {
+    get_pokemon(input_name);
+}
+
+fn get_pokemon(name: &str) {
+    match make_request(name) {
         Err(e) => handle_error(e),
-        Ok(response)  => {
-            let mut data = response;
-            print_info(&mut data);
+        Ok(pokemon)  => {
+            let mut data = pokemon;
+            print_pokemon(&mut data);
         }
     }
 }
@@ -59,22 +73,51 @@ fn make_request(pokemon: &str) -> Result<Pokemon, reqwest::Error> {
     Ok(resp)
 }
 
-fn print_info(p: &mut Pokemon) {
+fn print_pokemon(p: &mut Pokemon) {
     println!("ID: {}", style(&p.id).cyan());
     println!("Name: {}", style(&p.name).magenta());
-    println!("Types: {}", style(get_types(&mut p.types)).magenta());
     println!("Height: {}m", p.height as f32/10.0);
     println!("Weight: {}kg", p.weight as f32/10.0);
+
+    let ptypes = get_pokemon_types(&mut p.types);
+    print_types(ptypes);
 }
 
-fn get_types(type_array: &mut Vec<TypeSlot>) -> String {
-    let mut type_string = String::new();
-    type_array.sort_by(|a, b| a.slot.cmp(&b.slot));
-    for t in type_array {
-        if t.slot > 1 { type_string.push_str(", ") }
-        type_string.push_str(&t.type_object.name);
+fn print_types(ptypes: Vec<String>) {
+    let msg = format!("Types: {}", style(ptypes.join(", ")).yellow());
+    println!("{}", msg);
+    match get_type_effectiveness(ptypes) {
+        Err(e) => handle_error(e),
+        Ok(res)  => {
+            if res.double_damage_from.is_some() {
+                print!("Double damage from: ");
+                for t in res.double_damage_from {
+                    //print!("{}", t["name"]);
+                }
+            }
+        }
     }
-    return type_string;
+
+}
+
+fn get_pokemon_types(type_array: &mut Vec<TypeSlot>) -> Vec<String> {
+    let mut types = Vec::new();
+    let temp = type_array;
+    temp.sort_by(|a, b| a.slot.cmp(&b.slot));
+    for t in temp {
+        let type_name = t.type_object.name.to_string();
+        types.push(type_name);
+    }
+    return types;
+}
+
+fn get_type_effectiveness(types: Vec<String>) -> Result<TypeEffectiveness, reqwest::Error> {
+    let mut resp: TypeEffectiveness = TypeEffectiveness::default();
+    for t in types {
+        let uri = format!("https://pokeapi.co/api/v2/type/{}", t);
+        resp = reqwest::get(&uri)?.json()?;
+    }
+    Ok(resp)
 }
 
 fn handle_error(e: reqwest::Error) {
